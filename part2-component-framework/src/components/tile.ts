e1be134e458c0352c3885e8e9e9d0fac95e49ce8
Šťovík <p.stovik@cz.gmc.net>
2019-10-18 21:31:03 +0200

    P2 - implement tile params

--- part2-component-framework/src/components/tile.ts
+++ part2-component-framework/src/components/tile.ts
@@ -5,70 +5,74 @@ export enum TileType {
 }
 
 export enum TileRotation {
-    R0,
-    R90,
-    R180,
-    R270
+    R0 = 0,
+    R90 = 90,
+    R180 = 180,
+    R270 = 270
 }
 
-export default function tile(_params: { type: TileType; rotation?: TileRotation }): string {
+export default function tile(params: { type: TileType; rotation?: TileRotation }): string {
+    const rotation = params.rotation === undefined ? TileRotation.R0 : params.rotation;
     return `
-<div class="tile"></div>
+        <div class="tile ${getTileCssClass(params.type)}" style="transform: rotate(${rotation}deg)"></div>
+        ${getCssStyles()}
+    `;
+}
 
-<style> 
-  .tile {
-    width: 32.5%;
-    padding-bottom: 32.5%; /* Same as width, sets height */
-    margin-bottom: 1.25%; /* (100-32.5*3)/2 */
-    position: relative;
-  
-    background-color: rgb(58, 179, 64);
-  }
-  
-  .tile::before, .tile::after {
-    content: "";
-    display: block;
-    position: absolute;
-  
-    /* https://leaverou.github.io/css3patterns/#carbon-fibre */
-    background:
-      radial-gradient(black 15%, transparent 16%) 0 0,
-      radial-gradient(black 15%, transparent 16%) 8px 8px,
-      radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
-      radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
-    background-color:#282828;
-    background-size:16px 16px;
-  }
-  
-  .tile.tile--straight::before {
-    width: 100%;
-    height: 40%;
-    top: 30%;
-  }
-  
-  .tile.tile--bend::before {
-    width: 70%;
-    height: 40%;
-    top: 30%;
-  }
-  
-  .tile.tile--bend::after {
-    width: 40%;
-    height: 70%;
-    left: 30%;
-  }
-  
-  .tile.tile--rotate-90 {
-    transform: rotate(90deg);
-  }
-  
-  .tile.tile--rotate-180 {
-    transform: rotate(180deg);
-  }
-  
-  .tile.tile--rotate-270 {
-    transform: rotate(270deg);
-  }  
-</style>
-`;
+function getTileCssClass(type: TileType): string {
+    switch (type) {
+        case TileType.Empty:
+            return "";
+        case TileType.Straight:
+            return "tile--straight";
+        case TileType.Bend:
+            return "tile--bend";
+    }
+    throw new Error(`Unknown type "${type}"`);
+}
+
+function getCssStyles(): string {
+    return `<style> 
+    .tile {
+      width: 32.5%;
+      padding-bottom: 32.5%; /* Same as width, sets height */
+      margin-bottom: 1.25%; /* (100-32.5*3)/2 */
+      position: relative;
+    
+      background-color: rgb(58, 179, 64);
+    }
+    
+    .tile::before, .tile::after {
+      content: "";
+      display: block;
+      position: absolute;
+    
+      /* https://leaverou.github.io/css3patterns/#carbon-fibre */
+      background:
+        radial-gradient(black 15%, transparent 16%) 0 0,
+        radial-gradient(black 15%, transparent 16%) 8px 8px,
+        radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+        radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+      background-color:#282828;
+      background-size:16px 16px;
+    }
+    
+    .tile.tile--straight::before {
+      width: 100%;
+      height: 40%;
+      top: 30%;
+    }
+    
+    .tile.tile--bend::before {
+      width: 70%;
+      height: 40%;
+      top: 30%;
+    }
+    
+    .tile.tile--bend::after {
+      width: 40%;
+      height: 70%;
+      left: 30%;
+    } 
+  </style>`;
 }
