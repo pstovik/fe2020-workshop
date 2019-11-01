81a3294bf3dfaa0849944ba7c51c18ff3359b34a
Petr Stovik <p.stovik@quadient.com>
2019-11-01 08:03:17 +0100

    P2 - simplify design to not break in electron

--- part2-component-framework/src/components/tile.ts
+++ part2-component-framework/src/components/tile.ts
@@ -49,9 +49,7 @@ framework.componentStyle(`
         /* https://leaverou.github.io/css3patterns/#carbon-fibre */
         background:
             radial-gradient(black 15%, transparent 16%) 0 0,
-            radial-gradient(black 15%, transparent 16%) 8px 8px,
-            radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
-            radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+            radial-gradient(black 15%, transparent 16%) 8px 8px;
         background-color:#282828;
         background-size:16px 16px;
     }
