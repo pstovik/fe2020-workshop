84176f1eb04f8946da85eaa1c7b7129ebd1d279e
Künzelová, Iva <i.kunzelova@quadient.com>
2019-11-01 12:36:35 +0100

    P3 - split tests

--- part3-tests-application/spec/stepCount.spec.ts
+++ part3-tests-application/spec/stepCount.spec.ts
@@ -2,22 +2,34 @@ import { GameBoard } from "../cypress/components/gameBoard";
 
 let gameArea;
 
-describe("My first test", () => {
-    
-    it("Check number of steps", () => {
-    gameArea = new GameBoard(3,3);
+beforeEach(() => {
+    cy.visit("/");
+    cy.reload()
+  });
 
-        //given
-        cy.visit("http://localhost:8080/")        
- 
+describe("Check number of steps", () => {
+
+    it("Non empty tiles", () => {
+    gameArea = new GameBoard(3,3);      
+
         //when
         gameArea.rotateTile(0,1)
         gameArea.rotateTile(0,2)
         gameArea.rotateTile(0,1)
         gameArea.rotateTile(0,2)
-        gameArea.rotateTile(2,0)
     
         //then
         gameArea.assertNumberOfSteps(4)   
+    });
+
+    it("Empty tiles", () => {
+    gameArea = new GameBoard(3,3);    
+
+        //when
+        gameArea.rotateTile(2,0)
+        gameArea.rotateTile(0,0)
+
+        //then
+        gameArea.assertNumberOfSteps(0)
     });
 });
