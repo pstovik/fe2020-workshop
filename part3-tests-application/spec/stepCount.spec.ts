f5cbbb2b44b7808542d50d2c094b625bdfca9be4
Petr Stovik <p.stovik@quadient.com>
2019-11-01 14:59:01 +0100

    P3 - custom board setup in test

--- part3-tests-application/spec/stepCount.spec.ts
+++ part3-tests-application/spec/stepCount.spec.ts
@@ -1,35 +1,49 @@
 import { GameBoard } from "../cypress/components/gameBoard";
+import { serializeTiles } from "../src/dependencies";
+import { TileType, TileRotation } from "../../part2-component-framework/src/iGameState";
 
 let gameArea;
 
 beforeEach(() => {
-    cy.visit("/");
-    cy.reload()
-  });
+    const tilesString = serializeTiles([
+        { type: TileType.Empty },
+        { type: TileType.StraightLR },
+        { type: TileType.BendLT, rotation: TileRotation.CW270 },
+
+        { type: TileType.Empty },
+        { type: TileType.StraightLR, rotation: TileRotation.CW90 },
+        { type: TileType.StraightLR, rotation: TileRotation.CW90 },
+
+        { type: TileType.Empty },
+        { type: TileType.StraightLR, rotation: TileRotation.CW90 },
+        { type: TileType.Empty }
+    ]);
+    cy.visit(`/#${tilesString}`);
+    cy.reload();
+});
 
 describe("Check number of steps", () => {
-
     it("Non empty tiles", () => {
-    gameArea = new GameBoard(3,3);      
+        gameArea = new GameBoard(3, 3);
 
         //when
-        gameArea.rotateTile(0,1)
-        gameArea.rotateTile(0,2)
-        gameArea.rotateTile(0,1)
-        gameArea.rotateTile(0,2)
-    
+        gameArea.rotateTile(0, 1);
+        gameArea.rotateTile(0, 2);
+        gameArea.rotateTile(0, 1);
+        gameArea.rotateTile(0, 2);
+
         //then
-        gameArea.assertNumberOfSteps(4)   
+        gameArea.assertNumberOfSteps(4);
     });
 
     it("Empty tiles", () => {
-    gameArea = new GameBoard(3,3);    
+        gameArea = new GameBoard(3, 3);
 
         //when
-        gameArea.rotateTile(2,0)
-        gameArea.rotateTile(0,0)
+        gameArea.rotateTile(2, 0);
+        gameArea.rotateTile(0, 0);
 
         //then
-        gameArea.assertNumberOfSteps(0)
+        gameArea.assertNumberOfSteps(0);
     });
 });
---
+++ part3-tests-application/src/dependencies.ts
@@ -0,0 +1,1 @@
+export * from "../../part2-component-framework/src/serialization";
